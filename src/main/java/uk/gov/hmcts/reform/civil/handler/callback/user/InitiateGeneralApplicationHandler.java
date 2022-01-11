ae67826d3d94345de876c09b8a073d2d10b830f2
smitatalawadekar-hmcts <smita.talawadekar@HMCTS.NET>
2022-01-11 09:49:03 +0000

    CIV-659 On about-to-submit call handling initial commit

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandler.java
@@ -0,0 +1,162 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.BusinessProcess;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.genapplication.ApplicationType;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
+import uk.gov.hmcts.reform.civil.model.genapplication.RespondentOrderAgreement;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
+import static org.springframework.util.CollectionUtils.isEmpty;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_GENERAL_APPLICATION;
+import static uk.gov.hmcts.reform.civil.enums.MultiPartyScenario.isMultiPartyScenario;
+import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
+
+@Service
+@RequiredArgsConstructor
+public class InitiateGeneralApplicationHandler extends CallbackHandler {
+
+    private static final String CONFIRMATION_SUMMARY = "<br/><p> Your Court will make a decision on %s."
+        + "<ul> %s </ul>"
+        + "</p> %s"
+        + " %s ";
+    private static final String URGENT_APPLICATION = "<p> You have marked this application as urgent. </p>";
+    private static final String PARTY_NOTIFIED = "<p> The other %s legal representative %s "
+        + "that you have submitted this application.";
+    private static final List<CaseEvent> EVENTS = Collections.singletonList(INITIATE_GENERAL_APPLICATION);
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
+            callbackKey(ABOUT_TO_SUBMIT), this::submitClaim,
+            callbackKey(SUBMITTED), this::buildConfirmation
+        );
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private CaseData.CaseDataBuilder getSharedData(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        // second idam call is workaround for null pointer when hiding field in getIdamEmail callback
+        CaseData.CaseDataBuilder dataBuilder = caseData.toBuilder();
+
+        dataBuilder.businessProcess(BusinessProcess.ready(INITIATE_GENERAL_APPLICATION));
+
+        return dataBuilder;
+    }
+
+    private CallbackResponse submitClaim(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        // second idam call is workaround for null pointer when hiding field in getIdamEmail callback
+        CaseData.CaseDataBuilder dataBuilder = getSharedData(callbackParams);
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(buildCaseData(dataBuilder, caseData).toMap(objectMapper))
+            .build();
+    }
+
+    private CaseData buildCaseData(CaseData.CaseDataBuilder dataBuilder, CaseData caseData) {
+        List<Element<GeneralApplication>> applications = addApplication(buildApplication(caseData),
+                                                                        caseData.getGeneralApplications());
+        return dataBuilder
+            .generalAppRespondentAgreement(RespondentOrderAgreement.builder().build())
+            .generalAppType(ApplicationType.builder().build())
+            .generalApplications(applications)
+            .build();
+    }
+
+    private GeneralApplication buildApplication(CaseData caseData) {
+        return GeneralApplication.builder()
+            .generalAppType(caseData.getGeneralAppType())
+            .generalAppRespondentAgreement(caseData.getGeneralAppRespondentAgreement())
+            .businessProcess(BusinessProcess.ready(INITIATE_GENERAL_APPLICATION))
+            .build();
+    }
+
+    private SubmittedCallbackResponse buildConfirmation(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        String body = buildConfirmationSummary(caseData);
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader("# You have made an application")
+            .confirmationBody(body)
+            .build();
+    }
+
+    private String buildConfirmationSummary(CaseData caseData) {
+        // Below is the mocked up data... we will get this information (like type(s) of application, multiparty or not,
+        // notified or not etc.) from the collection itself from the caseData.
+        // Until that object is ready, let's use a dummy one.
+        List<String> applicationTypes = Arrays.asList("Strike Out", "Summary Judgement", "Extend Time");
+        String collect = applicationTypes.stream().map(appType -> "<li>" + appType + "</li>")
+            .collect(Collectors.joining());
+        //TODO: Fix me
+        boolean isApplicationUrgent = Optional.of(isEmpty(caseData.getGeneralApplications())).orElse(true);
+        boolean isMultiParty = isMultiPartyScenario(caseData);
+        //TODO: Fix me
+        boolean isNotified = Optional.of(caseData.getGeneralAppRespondentAgreement()==null).orElse(true);
+        String lastLine = format(PARTY_NOTIFIED, isMultiParty ? "parties'" : "party's",
+                                        isNotified ? "has been notified" : "has not been notified");
+        return format(CONFIRMATION_SUMMARY,
+                             applicationTypes.size() == 1 ? "this application" : "these applications",
+                             collect,
+                             isApplicationUrgent ? URGENT_APPLICATION : " ",
+                             lastLine
+                             );
+    }
+
+    /**
+     * To be used to return empty callback response, will be used in overriding classes.
+     *
+     * @param callbackParams This parameter is required as this is passed as reference for execute method in CallBack
+     * @return empty callback response
+     */
+    protected CallbackResponse emptyCallbackResponse(CallbackParams callbackParams) {
+        return AboutToStartOrSubmitCallbackResponse.builder().build();
+    }
+
+    /**
+     * Returns empty submitted callback response. Used by events that set business process to ready, but doesn't have
+     * any submitted callback logic (making callback is still required to trigger EventEmitterAspect)
+     *
+     * @param callbackParams This parameter is required as this is passed as reference for execute method in CallBack
+     * @return empty submitted callback response
+     */
+    protected CallbackResponse emptySubmittedCallbackResponse(CallbackParams callbackParams) {
+        return SubmittedCallbackResponse.builder().build();
+    }
+
+    public List<Element<GeneralApplication>> addApplication(GeneralApplication application,
+                                                            List<Element<GeneralApplication>>
+                                                                generalApplicationDetails) {
+        List<Element<GeneralApplication>> newApplication = ofNullable(generalApplicationDetails).orElse(newArrayList());
+        newApplication.add(element(application));
+
+        return newApplication;
+    }
+}
